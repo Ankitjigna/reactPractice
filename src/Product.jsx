39fec74ef6f6adb974cd836f09bb5ee5b2b50be3
Ankitjigna <ankitjigna01@gmail.com>
2024-10-11 20:28:35 +0530

    passing arrays to props

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,12 @@
 import "./Product.css"
 
-function Product({Title ,Price}) {
+function Product({Title ,Price ,features,features1 }) {
     return (
         <div className="Product">
             <h2>{Title}</h2>
             <h3>Price = {Price}</h3>
+            <h4>features = {features}</h4>
+            <h4>features1 = {features1.b}</h4>
         </div>
     )
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,12 +1,14 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
+  let features = ["No hang","durable"];
+  let option = {a:"No hang",b:"durable"};
   return (
     <>
-      <Product Title="Phone"  Price={30000}/>
-      <Product Title="Laptop" Price={98000}/>
+      <Product Title="Phone"  Price={30000} features={features} features1={option}/>
+      {/* <Product Title="Laptop" Price={98000}/>
       <Product Title="Camera" Price={50000}/>
-      <Product Title="Tablet" Price={60000}/>
+      <Product Title="Tablet" Price={60000}/> */}
     </>
   )
 }
