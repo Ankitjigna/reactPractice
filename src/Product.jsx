5ee9bb8ec98a963fa663baf8dc03313210f56fe2
Ankitjigna <ankitjigna01@gmail.com>
2024-10-11 20:38:00 +0530

    rendering arrays

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,12 @@
 import "./Product.css"
 
-function Product({Title ,Price ,features,features1 }) {
+function Product({Title ,Price ,features }) {
+    const list = features.map((feature)=><li>{feature}</li>)
     return (
         <div className="Product">
             <h2>{Title}</h2>
             <h3>Price = {Price}</h3>
-            <h4>features = {features}</h4>
-            <h4>features1 = {features1.b}</h4>
+            <h4>features = {list}</h4>
         </div>
     )
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -2,10 +2,10 @@ import Product from "./Product.jsx";
 
 function ProductTab() {
   let features = ["No hang","durable"];
-  let option = {a:"No hang",b:"durable"};
+  
   return (
     <>
-      <Product Title="Phone"  Price={30000} features={features} features1={option}/>
+      <Product Title="Phone"  Price={30000} features={features}/>
       {/* <Product Title="Laptop" Price={98000}/>
       <Product Title="Camera" Price={50000}/>
       <Product Title="Tablet" Price={60000}/> */}
