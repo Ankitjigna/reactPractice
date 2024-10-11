ac57e06c10f9d115b02a6750e58c5ca27f67e8f1
Ankitjigna <ankitjigna01@gmail.com>
2024-10-12 00:08:41 +0530

    adding conditionals and dynamic component style

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,15 @@
 import "./Product.css"
 
 function Product({Title ,Price ,features }) {
-    const list = features.map((feature)=><li>{feature}</li>)
+    // const list = features.map((feature)=><li>{feature}</li>)
+    let style = {backgroundColor: Price>=60000 ? "pink":null};
     return (
-        <div className="Product">
+        <div className="Product" style={style}>
             <h2>{Title}</h2>
             <h3>Price = {Price}</h3>
-            <h4>features = {list}</h4>
+            {Price>=60000 && <p>Discount of 5%</p> }
+            {/* {Price>=60000 ? <p>Discount of 5%</p> : null} */}
+            {/* <h4>features = {list}</h4> */}
         </div>
     )
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,14 +1,14 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
-  let features = ["No hang","durable"];
+  // let features = ["No hang","durable"];
   
   return (
     <>
-      <Product Title="Phone"  Price={30000} features={features}/>
-      {/* <Product Title="Laptop" Price={98000}/>
+      {/* <Product Title="Phone"  Price={30000} features={features}/> */}
+      <Product Title="Laptop" Price={98000}/>
       <Product Title="Camera" Price={50000}/>
-      <Product Title="Tablet" Price={60000}/> */}
+      <Product Title="Tablet" Price={60000}/> 
     </>
   )
 }
