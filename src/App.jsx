5338f0692a6b0b74f9351e8c2d5ace49ac0247a3
Ankitjigna <ankitjigna01@gmail.com>
2024-10-14 15:28:57 +0530

    event object in handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import ProductTab from "./ProductTab.jsx";
-import Button from "./button.jsx";
+import Form from "./form.jsx";
 function App() {
   return (
     <>
-    <Button/>
-    <h2>Amazing deals | go for it</h2>
-      <ProductTab/>
+    <Form/>
     </>
   );
 }
